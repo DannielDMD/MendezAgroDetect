20c60fc8fb9012552f95cd14d618fec052feaf6b
Daniel Méndez <desmendezd@gmail.com>
2024-11-18 21:00:50 -0500

    Ajustes para despliegue en Rende

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def send_result_image(filename):
     return app.send_static_file(filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
